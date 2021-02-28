ef2fa75579e5fe6baf73c531eed2ee6340a152d1
TsMax <QinYUN575@Gmail.com>
2021-03-01 04:16:10 +0800

    Update examples of LicheePi Nano

---
+++ framebuffer_demo/readme.md
@@ -0,0 +1,1 @@
+convert -resize 480x272 -depth 8 ./wallpaper.jpg wallpaper.bmp
---
+++ helloworld/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("====================\r\n");
+    printf("%s:%s\r\n", __DATE__, __TIME__);
+    printf("====================\r\n");
+    printf("Hello World!\r\n");
+    return 0;
+}
